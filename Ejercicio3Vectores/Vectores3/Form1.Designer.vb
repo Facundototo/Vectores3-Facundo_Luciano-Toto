430d5c7b964c0710e813b0a1ab6b01080e193049
Facundototo <81201231+Facundototo@users.noreply.github.com>
2022-11-21 20:53:21 -0300

    Add files via upload

---
+++ Ejercicio3Vectores/Vectores3/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim ListViewItem1 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("")
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Codigo de alimento"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(121, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre de comedor:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(96, 60)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cantidad:"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(160, 7)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {9999, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(37, 23)
+        Me.NumericUpDown1.TabIndex = 3
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(203, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 15)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Alimento inexistente"
+        Me.Label4.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(96, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Cargar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(160, 58)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {999999, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(83, 23)
+        Me.NumericUpDown2.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(160, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(96, 116)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(164, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Finalizar entrada de datos"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem1})
+        Me.ListView1.Location = New System.Drawing.Point(12, 181)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(390, 178)
+        Me.ListView1.TabIndex = 9
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 371)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListView1 As ListView
+End Class
---
+++ Ejercicio3Vectores/Vectores3/Form1.vb
@@ -0,0 +1,92 @@
+﻿Imports System.Net.Sockets
+
+Public Class Form1
+    Dim Alimentos() As Integer = {10, 110, 1, 2, 7, 24, 88, 99, 12, 51}
+    Dim precio() As Integer = {66, 255, 40, 99, 1999, 23, 72, 31, 84, 23}
+    Dim cont As Integer = 0
+    Dim contali(10) As Integer
+    Dim acumali(10) As Integer
+
+    Structure Estructura
+        Dim Alimento As Integer
+        Dim Comedor As String
+        Dim Cant As Integer
+    End Structure
+    Dim Comida(25) As Estructura
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Dim sino As Boolean = False
+        For i As Integer = 0 To 9
+            If NumericUpDown1.Value = Alimentos(i) Then
+                sino = True
+            End If
+        Next
+
+        If sino = False Then
+            Label4.Visible = True
+            Button1.Enabled = False
+        Else
+            Label4.Visible = False
+            Button1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ReDim Preserve Comida(cont + 1)
+        cont += 1
+        Comida(cont).Alimento = NumericUpDown1.Value
+        Comida(cont).Comedor = TextBox1.Text
+        Comida(cont).Cant = NumericUpDown2.Value
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button1.Enabled = False
+        Button2.Enabled = False
+        For i As Integer = 0 To cont
+            Select Case Comida(i).Alimento
+                Case 10
+                    contali(0) += 1
+                    acumali(0) += Comida(i).Cant
+                Case 110
+                    contali(1) += 1
+                    acumali(1) += Comida(i).Cant
+                Case 1
+                    contali(2) += 1
+                    acumali(2) += Comida(i).Cant
+                Case 2
+                    contali(3) += 1
+                    acumali(3) += Comida(i).Cant
+                Case 7
+                    contali(4) += 1
+                    acumali(4) += Comida(i).Cant
+                Case 24
+                    contali(5) += 1
+                    acumali(5) += Comida(i).Cant
+                Case 88
+                    contali(6) += 1
+                    acumali(6) += Comida(i).Cant
+                Case 99
+                    contali(7) += 1
+                    acumali(7) += Comida(i).Cant
+                Case 12
+                    contali(8) += 1
+                    acumali(8) += Comida(i).Cant
+                Case 51
+                    contali(9) += 1
+                    acumali(9) += Comida(i).Cant
+            End Select
+
+        Next
+        ListView1.Items.Add("Alimentos totales: ")
+        For i As Integer = 0 To 9
+            Dim preciototal As Integer = acumali(i) * precio(i)
+            ListView1.Items.Add("Cantidad del alimento " & Alimentos(i) & " " & acumali(i) & " Precio: " & preciototal)
+        Next
+        ListView1.Items.Add("Alimentos individuales: ")
+
+        For i As Integer = 0 To cont
+            ListView1.Items.Add(Comida(i).Comedor + ": " & Comida(i).Cant)
+        Next
+
+    End Sub
+
+End Class
